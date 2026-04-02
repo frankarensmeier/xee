aa52d53345c2f5d6ae9fc7ea1d936246b5c98c0e
Frank Arensmeier <frank@paligo.se>
2026-04-02 23:17:50 +0200

    Updated supported tests

--- xee-xslt-compiler/tests/test_xslt.rs
+++ xee-xslt-compiler/tests/test_xslt.rs
@@ -2665,4 +2665,43 @@ fn test_vendor_mode_0015_on_no_match_with_attributes() {
     assert_eq!(result, expected, "mode-0015: Output mismatch");
 }
 
+#[test]
+fn test_vendor_mode_0007_direct_attribute_text_only_copy() {
+    let mut xot = Xot::new();
+    let output = evaluate(
+        &mut xot,
+        "<dummy/>",
+        r##"
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="3.0"
+  xmlns:xs="http://www.w3.org/2001/XMLSchema"
+  exclude-result-prefixes="xs">
+
+  <xsl:mode name="c" on-no-match="shallow-copy"/>
+  <xsl:mode name="d" on-no-match="shallow-skip"/>
+  <xsl:mode name="s" on-no-match="text-only-copy"/>
+
+  <xsl:variable name="temp" as="attribute()">
+    <xsl:attribute name="a">abracadabra</xsl:attribute>
+  </xsl:variable>
+
+  <xsl:template match="/">
+    <xsl:call-template name="main"/>
+  </xsl:template>
+
+  <xsl:template name="main">
+    <out>
+      <c><xsl:apply-templates select="$temp" mode="c"/></c>
+      <d><xsl:apply-templates select="$temp" mode="d"/></d>
+      <s><xsl:apply-templates select="$temp" mode="s"/></s>
+    </out>
+  </xsl:template>
+</xsl:stylesheet>"##,
+    )
+    .unwrap();
+
+    let result = xml(&xot, output);
+    let expected = "<out><c a=\"abracadabra\"/><d/><s>abracadabra</s></out>";
+    assert_eq!(result, expected, "mode-0007: Output mismatch");
+}
+
 
